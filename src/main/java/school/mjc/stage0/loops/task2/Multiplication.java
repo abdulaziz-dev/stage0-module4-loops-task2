033870ddbd4ec980b163615492699e2f51963b6b
abdulaziz-dev <axmedovabdulaziz2001@gmail.com>
2023-02-23 12:26:58 +0500

    brainteasers_final_update

--- src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
+++ src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
@@ -4,7 +4,7 @@ public class Multiplication {
     public void printMultiplied(int multiplyByAndToInclusive) {
         int i =0;
         while (i <= Math.abs(multiplyByAndToInclusive)){
-            System.out.println(i * Math.abs(multiplyByAndToInclusive));
+            System.out.println(i * multiplyByAndToInclusive);
             i += 1;
         }
     }
--- src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
+++ src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
@@ -4,7 +4,7 @@ public class WordsBuilder {
     public void buildPhrase(char... chars) {
         int i=0;
         while (i < chars.length){
-            System.out.print(i);
+            System.out.print(chars[i]);
             i += 1;
         }
     }
